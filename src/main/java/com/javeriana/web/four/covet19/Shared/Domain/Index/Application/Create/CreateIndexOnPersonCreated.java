b10cbb5711dfb5bde1574aacc751022fdd0be898
Jairo Vanegas <jairo.vanegas@javeriana.edu.co>
2021-06-02 12:40:13 -0500

    Actualizado el CORS

--- src/main/java/com/javeriana/web/four/covet19/Shared/Domain/Index/Application/Create/CreateIndexOnPersonCreated.java
+++ src/main/java/com/javeriana/web/four/covet19/Shared/Domain/Index/Application/Create/CreateIndexOnPersonCreated.java
@@ -3,6 +3,7 @@ package com.javeriana.web.four.covet19.Shared.Domain.Index.Application.Create;
 import com.javeriana.web.four.covet19.Shared.Domain.Admin.PersonCreatedDomainEvent;
 import com.javeriana.web.four.covet19.Shared.Domain.Bus.Event.DomainEventSubscriber;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 
 @DomainEventSubscriber({PersonCreatedDomainEvent.class})
 public class CreateIndexOnPersonCreated {
@@ -13,6 +14,7 @@ public class CreateIndexOnPersonCreated {
         this.createIndex = createIndex;
     }
 
+    @Async
     @EventListener
     public void on(PersonCreatedDomainEvent event) {
         this.createIndex.execute(event.getEmail(), event.aggregateId(), event.getRol());
--- src/main/java/com/javeriana/web/four/covet19/Shared/Infrastructure/Security/WebSecurityConfig.java
+++ src/main/java/com/javeriana/web/four/covet19/Shared/Infrastructure/Security/WebSecurityConfig.java
@@ -6,10 +6,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
